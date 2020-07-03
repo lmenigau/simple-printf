71c846c5e0ef58b178eff297bebf9884502eb46e
Louis MENIGAULT <lomeniga@e1r10p16.42.fr>
2020-07-03 05:08:26 +0200

    kind of compile

--- ft_printf.c
+++ ft_printf.c
@@ -6,38 +6,74 @@
 /*   By: lomeniga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 22:26:29 by lomeniga          #+#    #+#             */
-/*   Updated: 2020/07/01 23:15:09 by lomeniga         ###   ########.fr       */
+/*   Updated: 2020/07/03 05:07:55 by lomeniga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
 #include "ft_printf.h"
 
-int		parse_format(const char *fmt, va_list ap)
+int		(*(g_parse[256]))(unsigned char *, t_parse *) = {
+	['1'] = parse_field,
+	['2'] = parse_field,
+	['3'] = parse_field,
+	['4'] = parse_field,
+	['5'] = parse_field,
+	['6'] = parse_field,
+	['7'] = parse_field,
+	['8'] = parse_field,
+	['9'] = parse_field,
+	['c'] = conv_char,
+	['s'] = conv_string,
+	['p'] = conv_ptr,
+	['d'] = conv_int,
+	['i'] = conv_int,
+	['u'] = conv_uns,
+	['x'] = conv_hex,
+	['X'] = conv_hex,
+	['%'] = conv_pc,
+	['-'] = flag_min,
+	['0'] = flag_zero,
+	['.'] = flag_prec,
+	['*'] = flag_aste,
+};
+
+void	parse_format(unsigned char *fmt, va_list ap)
 {
-	char 	c;
+	t_parse	parse;
 
-
-	if (*fmt == '%')
-		write(1, "%", 1);
-	else if (*fmt == 'c')
+	va_copy(parse.ap, ap);
+	while (g_parse[(int)*fmt])
 	{
-		c = va_arg(ap, char); 
+		g_parse[(int)*fmt](fmt, &parse);
+		fmt++;
 	}
 }
 
 int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
+	int			count;
 
-	va_start(ap)
+	count = 0;
+	va_start(ap, format);
 	while (*format)
 	{
-		if (*format == '%');
-			parse_format(format + 1, ap);
+		if (*format == '%')
+			parse_format((unsigned char *)format + 1, ap);
 		else
+		{
 			write(1, format, 1);
+			count++;
+		}
 		format++;
 	}
 	va_end(ap);
+	return (count);
+}
+
+int		main(void)
+{
+	ft_printf("%c", 'c', 1.5f);
 }
