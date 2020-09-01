315f5d747f8823cebde3790971de0d1ae98e31c3
Louis MENIGAULT <lomeniga@e1r10p8.42.fr>
2020-09-01 11:06:32 +0200

    test 0prec 0

--- test.c
+++ test.c
@@ -3,8 +3,8 @@
 int		main(void)
 {
 	int		a = 3;
-	ft_printf("{%10.5d}\n", 10, 1);
-	printf("{%10.5d}\n", 10);
+	ft_printf("{%.d}\n", 0);
+	printf("{%.d}\n", 0);
 	ft_printf("{%-10s}\n", __func__);
 	printf("{%-10s}\n", __func__);
 }
